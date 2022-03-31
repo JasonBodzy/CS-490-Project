a8696d70f1d409a0d47465e4b2b877a70832d01e
Jason Bodzy <jasonbodzy@gmail.com>
2022-03-30 23:22:54 -0400

    Basic login screen
    Followed SwiftUI tutorial for basic frontend screen -no backend yet.

--- Philanthrofeed/ContentView.swift
+++ Philanthrofeed/ContentView.swift
@@ -4,13 +4,28 @@
 //
 //  Created by Jason Bodzy on 3/30/22.
 //
-
+// If you tapp on the Login text it transitions to login screen
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapped = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if !tapped {
+                Text("Login")
+                    .padding()
+            } else {
+                LoginView()
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                if (!tapped) {
+                    tapped.toggle()
+                }
+            }
+        }
+        
     }
 }
 
---
+++ Philanthrofeed/LoginView.swift
@@ -0,0 +1,59 @@
+//
+//  LoginView.swift
+//  Philanthrofeed
+//
+//  Created by Jason Bodzy on 3/30/22.
+//
+// Followed tutorial for basic login UI stuff
+import SwiftUI
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    var body: some View {
+        VStack {
+            WelcomeText()
+            TextField("Username", text: $username)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            TextField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Button(action: {print("Button tapped")}) {
+                LoginButtonContent()
+            }
+        }
+        .padding()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
