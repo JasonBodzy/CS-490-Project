67ac1fe94ae3248e656d73a037e7a881aa66da17
Yatin Kukreja <ykukreja@purdue.edu>
2022-04-04 13:54:59 -0700

    Added app delegate, still need to add keys

--- Philanthrofeed/PhilanthrofeedApp.swift
+++ Philanthrofeed/PhilanthrofeedApp.swift
@@ -7,8 +7,19 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = Nil) -> Bool {
+        
+        return true
+    }
+}
+
 @main
+
+
 struct PhilanthrofeedApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
