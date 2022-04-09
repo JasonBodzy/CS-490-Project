59fa2e80880b75edc1d9489ebf04cf31cd542c16
Yatin Kukreja <ykukreja@purdue.edu>
2022-04-09 12:53:39 -0700

    Added camera view, profile view, feed view and added a navbar and scrollview to feed view

---
+++ Philanthrofeed/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  Philanthrofeed
+//
+//  Created by user209496 on 4/9/22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text("Camera View")
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
---
+++ Philanthrofeed/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  profile view.swift
+//  Philanthrofeed
+//
+//  Created by user209496 on 4/9/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
