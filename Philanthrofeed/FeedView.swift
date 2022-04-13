1889ae9775e238b7568056b3c6834b4f7431e1cb
Jason Bodzy <jasonbodzy@gmail.com>
2022-04-13 16:35:13 -0400

    frontend navigation

---
+++ Philanthrofeed/FeedView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedView.swift
+//  Philanthrofeed
+//
+//  Created by Jason Bodzy on 4/6/22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
