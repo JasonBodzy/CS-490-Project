f9f21abafb98f3ee1a9bf7fd8a2b2d6ce9f92972
Yatin Kukreja <ykukreja@purdue.edu>
2022-04-15 14:38:14 -0700

    User can create posts but they dont show up on the feed yetd

--- Philanthrofeed/FeedView.swift
+++ Philanthrofeed/FeedView.swift
@@ -6,11 +6,10 @@
 //
 
 import SwiftUI
+import Parse
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
         NavBar()
     }
 }
